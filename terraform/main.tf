6a804debdd2f23c522074135c369e9e850128f8b
Jackson <hackisonjd@users.noreply.github.com>
2025-03-23 23:47:53 -0400

    set up node boilerplate

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,48 +18,63 @@ provider "proxmox" {
     pm_api_token_secret = var.pm_api_token_secret
 }
 
-# Create a new VM instance on Proxmox
-resource "proxmox_vm_qemu" "vm-instance" {
-    name = "vm-instance"
+# Create a highly available K3s cluster
+resource "proxmox_vm_qemu" "k3s-master" {
     target_node = "homeserver"
-    clone = "ubuntu-cloud"
-    full_clone = true
-    cores = 2
-    memory = 2048
-    os_type = "cloud-init"
-    scsihw = "virtio-scsi-pci"
-    ipconfig0 = "ip=dhcp"
-    ciuser = var.user
-    cipassword = var.password
-    ciupgrade = true
-    sshkeys = join("\n", var.ssh_keys)
-
-
-
-    disk {
-        slot = "scsi0"
-        size = "32G"
-        type = "disk"
-        storage = "local-zfs"
-        discard = "true"
-    }
-
-    disk {
-        slot = "ide0"
-        type = "cloudinit"
-        storage = "local-zfs"
-    }
-
-    network {
-        id = 0
-        model = "virtio"
-        bridge = "vmbr0"
-        firewall = false
-        link_down = false
-    }
-
-    serial {
-        id = 0
-        type = "socket"
-    }
+    desc = "K3s master nodes"
+    count = 3
+    onboot = true
 }
+
+resource "proxmox_vm_qemu" "k3s-worker" {
+    target_node = "homeserver"
+    desc = "K3s worker nodes"
+    count = 2
+    onboot = true
+}
+
+# # Create a new VM instance on Proxmox
+# resource "proxmox_vm_qemu" "vm-instance" {
+#     name = "vm-instance"
+#     target_node = "homeserver"
+#     clone = "ubuntu-cloud"
+#     full_clone = true
+#     cores = 2
+#     memory = 2048
+#     os_type = "cloud-init"
+#     scsihw = "virtio-scsi-pci"
+#     ipconfig0 = "ip=dhcp"
+#     ciuser = var.user
+#     cipassword = var.password
+#     ciupgrade = true
+#     sshkeys = join("\n", var.ssh_keys)
+
+
+
+#     disk {
+#         slot = "scsi0"
+#         size = "32G"
+#         type = "disk"
+#         storage = "local-zfs"
+#         discard = "true"
+#     }
+
+#     disk {
+#         slot = "ide0"
+#         type = "cloudinit"
+#         storage = "local-zfs"
+#     }
+
+#     network {
+#         id = 0
+#         model = "virtio"
+#         bridge = "vmbr0"
+#         firewall = false
+#         link_down = false
+#     }
+
+#     serial {
+#         id = 0
+#         type = "socket"
+#     }
+# }
